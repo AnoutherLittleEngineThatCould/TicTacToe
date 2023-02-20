be6d783aeb8ee08cb314b9731fbc6c7c044780cb
Vovka <vvl@mail.ru>
2023-02-20 23:05:04 +0700

    Impliment ramdom computer move

--- src/ThatCould/AnoutherLittleEngine/tictactoe/component/ComputerMove.java
+++ src/ThatCould/AnoutherLittleEngine/tictactoe/component/ComputerMove.java
@@ -16,7 +16,10 @@
 
 package ThatCould.AnoutherLittleEngine.tictactoe.component;
 
+import ThatCould.AnoutherLittleEngine.tictactoe.model.Cell;
 import ThatCould.AnoutherLittleEngine.tictactoe.model.GameTable;
+
+import java.util.Random;
 
 /**
  * @author AnoutherLittleEngineThatCould
@@ -24,6 +27,15 @@ import ThatCould.AnoutherLittleEngine.tictactoe.model.GameTable;
  */
 public class ComputerMove {
     public void make(final GameTable gameTable) {
-
+        final Random random = new Random();
+        while (true) {
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, '0');
+                break;
+            }
+        }
     }
 }
